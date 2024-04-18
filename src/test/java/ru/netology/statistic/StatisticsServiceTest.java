fb3b306f2a3cb6cc27329de8278a5eab3f58f2ca
Maria Gamzina <gamzinamaria@gmail.com>
2024-04-18 22:50:57 +0300

    add code

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,16 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxIfGreater() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 5, 8, 4, 5, 3, 8, 6, 11, 11, 10};
+        long expected = 11;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
